566033edf97dc29e26ede91d2ddc23851dd27e95
Jackanhle <lejack164@gmail.com>
2022-04-04 20:12:21 +1000

    Update Anh's work

--- Anh's work/intelli meals/script.js
+++ Anh's work/intelli meals/script.js
@@ -12,7 +12,7 @@ searchForm.addEventListener('submit', (e) => {
     fetchAPI();
 });
 async function fetchAPI () {
-    const baseUrl = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_Key}`
+    const baseUrl = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_Key}`
     const response = await fetch(baseUrl);
     const data = await response.json();
     generateHTML(data.hits);
@@ -24,13 +24,16 @@ function generateHTML(results) {
         generatedHTML += 
         `
         <div class="item">
-        <img src="${result.recipe.imgage}" alt="">
+        <img src="${result.recipe.image}" alt="">
         <div class="flex-container">
           <h1 class="title">${result.recipe.label}</h1>
-          <a class="view-btn" href="#">Get Reciepe</a>
+          <a class="view-btn" href="${result.recipe.url}"target="_blank">Get Reciepe</a>
         </div>
         <p class="item-data">Calories:${result.recipe.calories.toFixed(2)}</p>
-        
+        <p class="item-data">MealType:${result.recipe.mealType}</p>
+        <p class="item-data">Diet Label:${result.recipe.dietLabels.length > 0? result.recipe.dietLabels : 'No Data Found'}</p>
+        <p class="item-data">Health Label:${result.recipe.healthLabels}</p>
+
         </div>
         
         `
